f8bff059e0be4c145ca72360692d7845e785ad75
umeneri <koishimst@gmail.com>
2018-09-22 10:46:17 +0900

    watcherのローカル実行

--- service-watcher/handler.py
+++ service-watcher/handler.py
@@ -1,9 +1,5 @@
-import datetime
-import urllib.request
+import wacher
 
+# Lambdaはこの関数を呼び出す
 def hello(event, context):
-    started_at = datetime.datetime.now()
-    with urllib.request.urlopen("https://example.com"):
-        ended_at = datetime.datetime.now()
-        elapsed = (ended_at - started_at).total_seconds()
-        return f'Time: {elapsed} seconds'
+    watcher.watch("https://example.com")
